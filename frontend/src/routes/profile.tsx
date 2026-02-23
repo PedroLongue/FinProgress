93ed0f595c7db158ae68e6b5b2ccf7ceca9a1cc8
PedroLongue <pedrolongue13@gmail.com>
2026-02-23 16:18:44 -0300

    fix import

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Profile from "../pages/Profile";
+import { Profile } from "../pages/Profile";
 
 export const Route = createFileRoute("/profile")({
   component: Profile,
