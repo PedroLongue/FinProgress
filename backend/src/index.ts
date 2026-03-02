b231fa90f0fdff7bb4b96e4b766719d2cbd44c60
PedroLongue <pedrolongue13@gmail.com>
2026-03-02 17:44:14 -0300

    refactor: update Dockerfile and index.ts for Prisma and CORS configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,9 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL
+      ? [process.env.FRONTEND_URL, "http://localhost:5173"]
+      : ["http://localhost:5173"],
     credentials: true,
   }),
 );
